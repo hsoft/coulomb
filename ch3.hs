9bef526c0483949f4e72760880752532411419e8
Virgil Dupras <hsoft@hardcoded.net>
2017-10-04 17:18:51 -0400

    ch3 s4

--- ch3.hs
+++ ch3.hs
@@ -4,6 +4,7 @@ import Geo
 
 densiteSurfacique solide charge = charge / (surface solide)
 
+chargeSelonDensiteLineique longueur densite = densite * longueur
 chargeSelonDensiteSurfacique solide densite = densite * (surface solide)
 
 -- args: solide chargé, volume creux?, charge, surface de gauss
@@ -22,3 +23,14 @@ s3 gauss =
 s3a = s3 (Sphere 0.1)
 s3b = s3 (Sphere 0.4)
 s3c = s3 (Sphere 0.3)
+
+s4 =
+    let lt = 0.5
+        tige = Cylindre 0 lt
+        champ = 7.193 * 10 ** (6) -- N/C
+        lg = 0.1
+        gauss = Cylindre 0.08 lg
+        qin = champ * (surface gauss) * ε0
+        densite = qin / lg
+        charge = chargeSelonDensiteLineique lt densite
+    in charge
