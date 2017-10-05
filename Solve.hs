06c79d7a4f4f05cb5d18321d2d63abd91f2aee18
Virgil Dupras <hsoft@hardcoded.net>
2017-10-05 16:02:56 -0400

    Solve: on se débarrasse de TermS

--- Solve.hs
+++ Solve.hs
@@ -1,17 +1,14 @@
 import Text.Printf
 
-data Term = TermS String | TermI Int String | TermF Float String
+data Term = TermI Int String | TermF Float String
 
 instance Show Term where
-    show (TermS id) = id
-    show (TermI mult id) = printf "%d%s" mult id
+    show (TermI mult id) = if mult == 1 then id else printf "%d%s" mult id
     show (TermF mult id) = printf "%0.2e%s" mult id
 
 instance Eq Term where
-    TermS x == TermS y = x == y
     TermI mx x == TermI my y = (x == y) && (mx == my)
     TermF mx x == TermF my y = (x == y) && (mx == my)
-    TermS x == TermI my y = (x == y) && (my == 1)
     _ == _ = False
 
 instance Ord Term where
@@ -55,40 +52,27 @@ data MergeResult = Merged Expr | NotMerged
 
 c x = (Const x)
 var t = if isTermNull t then c 0 else (Var t)
-vs x = (var (TermS x))
+vs x = vi 1 x
 vi mx x = (var (TermI mx x))
 add x y = (BinOp Add x y)
 mult x y = (BinOp Mult x y)
 div_ x y = (BinOp Div x y)
 equal x y = (BinOp Equal x y)
 
-negateTerm (TermS x) = (TermI (-1) x)
 negateTerm (TermI mx x) = (TermI (-mx) x)
 negateTerm (TermF mx x) = (TermF (-mx) x)
 
-canMergeTerm (TermS x) (TermS y) = x == y
-canMergeTerm (TermS x) (TermI _ y) = x == y
-canMergeTerm (TermS x) (TermF _ y) = x == y
-canMergeTerm (TermI _ x) (TermI _ y) = x == y
-canMergeTerm (TermI _ x) (TermF _ y) = x == y
-canMergeTerm (TermF _ x) (TermF _ y) = x == y
-canMergeTerm x y = canMergeTerm y x
+canMergeTerm x y = termIdent x == termIdent y
 
-termIdent (TermS x) = x
 termIdent (TermI _ x) = x
 termIdent (TermF _ x) = x
 
-termMult (TermS _) = 1
 termMult (TermI m _) = fromIntegral m
 termMult (TermF m _) = m
 
 isTermNull (TermI m _) = m == 0
 isTermNull (TermF m _) = m == 0.0
-isTermNull _ = False
 
-addTerm (TermS x) (TermS y) = (TermI 2 y)
-addTerm (TermS x) (TermI my y) = (TermI (my + 1) y)
-addTerm (TermS x) (TermF my y) = (TermF (my + 1) y)
 addTerm (TermI mx x) (TermI my y) = (TermI (mx + my) y)
 addTerm (TermI mx x) (TermF my y) = (TermF ((fromIntegral mx) + my) y)
 addTerm (TermF mx x) (TermF my y) = (TermF (mx + my) y)
